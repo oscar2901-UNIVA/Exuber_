100c1ed62cea3212da9edf2ef67edb73f98ec794
oscar2901-UNIVA <oscarj2901@gmail.com>
2024-01-17 12:52:37 -0600

    feat: complete readme and login and sign up

---
+++ README.md
@@ -0,0 +1,42 @@
+# EXUBER
+
+EXUBER es una aplicación que permite a los usuarios ordenar alimentos o bebidas en un establecimiento antes de llegar al lugar. Facilita el proceso de pedido y pago, brindando una experiencia más rápida y conveniente.
+
+## Características Principales
+
+- **Registro de Usuarios:** Los usuarios pueden crear cuentas para acceder a la aplicación.
+
+- **Administrador:** Los administradores pueden ver la lista de usuarios registrados.
+
+- **Pedido Anticipado:** Los usuarios pueden realizar pedidos anticipados y realizar pagos antes de llegar al establecimiento.
+
+## Instalación
+
+Para utilizar la aplicación, sigue estos pasos:
+
+1. **Clona el Repositorio:**
+   ```bash
+   git clone https://github.com/oscar2901-UNIVA/Exuber_.git
+
+2. Instala Dependencias:
+    cd exuber
+    npm install
+
+3. Ejecuta la Aplicación:
+    npm start
+
+
+## Uso
+1. Registro de Usuarios:
+    Abre la aplicación y crea una cuenta como usuario.
+
+2. Administrador:
+    Inicia sesión como administrador para acceder a la lista de usuarios registrados.
+
+3. Realizar Pedidos:
+    Inicia sesión como usuario y realiza pedidos anticipados.
+
+
+## Contribución
+    ¡Contribuciones son bienvenidas! Si encuentras algún problema o tienes alguna mejora, por favor abre un problema o envía un pull request.
+
--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,75 +1,104 @@
-import React, { useEffect } from "react";
-import { Keyboard, TouchableWithoutFeedback, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Keyboard, TouchableWithoutFeedback, View, Text } from "react-native";
 import { useForm, Controller } from "react-hook-form";
-import PasswordInput from "../../components/atoms/inputs/PasswordInput"; // Asegúrate de importar el componente PasswordInput desde la ubicación correcta
+import PasswordInput from "../../components/atoms/inputs/PasswordInput";
 import ButtonPrimary from "../../components/atoms/buttons/ButtonPrimary";
 import InputPrimary from "../../components/atoms/inputs/InputPrimary";
 import { useNavigation } from "@react-navigation/native";
-import { useDispatch } from "react-redux";
-import { getUsers } from "../../reduxStore/features/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getUsers,
+  login,
+  selectUsers,
+} from "../../reduxStore/features/user/userSlice";
 import SimpleLayout from "../../components/layouts/SimpleLayout";
 
 export default function Login() {
   const { control, handleSubmit, setValue } = useForm();
   const navigation = useNavigation();
-
   const dispatch = useDispatch();
+  const userState = useSelector(selectUsers);
 
   useEffect(() => {
     dispatch(getUsers());
   }, []);
 
-  const onSubmit = (data) => {
-    /* console.log(data); */
-    navigation.navigate("Home");
+  const onSubmit = async (data) => {
+    try {
+      const resultAction = await dispatch(login(data));
+
+      console.log(
+        "🚀 ~ file: Login.js:29 ~ onSubmit ~ resultAction:",
+        resultAction
+      );
+      if (resultAction.payload.message === "Invalid login credentials") {
+        console.log(
+          "🚀 ~ file: Login.js:29 ~ onSubmit ~ resultAction:",
+          resultAction.payload.message
+        );
+      } else {
+        navigation.navigate("Home");
+      }
+
+      /* if (login.fulfilled.match(resultAction)) {
+        navigation.navigate("Home");
+      } else if (login.rejected.match(resultAction)) {
+        console.error("Error en el inicio de sesión:", resultAction.error);
+        if (resultAction.error && resultAction.error.message) {
+          console.error("Error específico:", resultAction.error.message);
+        }
+        console.error("Error general:", "Error de inicio de sesión");
+      } */
+    } catch (error) {
+      console.error("Error en el inicio de sesión:", error);
+      console.error("Error general:", "Error de inicio de sesión");
+    }
   };
 
   return (
     <SimpleLayout hasScroll={false} style={{ paddingHorizontal: 5 }}>
-       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={{ padding: 20 }}>
+          <Controller
+            control={control}
+            render={({ field }) => (
+              <InputPrimary
+                placeholder="Correo"
+                control={control}
+                name="email"
+                onChangeText={(text) => setValue("email", text)}
+              />
+            )}
+            name="email"
+            defaultValue=""
+          />
+          <Controller
+            control={control}
+            render={({ field }) => (
+              <PasswordInput
+                placeholder="Contraseña"
+                control={control}
+                name="password"
+                onChangeText={(text) => setValue("password", text)}
+              />
+            )}
+            name="password"
+            defaultValue=""
+          />
 
-       
-      <View style={{ padding: 20 }}>
-        <Controller
-          control={control}
-          render={({ field }) => (
-            <InputPrimary
-              placeholder="Correo"
-              control={control}
-              name="email"
-              onChangeText={(text) => setValue("email", text)}
-            />
-          )}
-          name="email"
-          defaultValue=""
-        />
-        <Controller
-          control={control}
-          render={({ field }) => (
-            <PasswordInput
-              placeholder="Contraseña"
-              control={control}
-              name="password"
-              onChangeText={(text) => setValue("password", text)}
-            />
-          )}
-          name="password"
-          defaultValue=""
-        />
-
-        <ButtonPrimary
-          title="login"
-          text="Iniciar sesión"
-          onPress={handleSubmit(onSubmit)}
-        />
-        <ButtonPrimary
-          title="register"
-          text="Registrarse"
-          onPress={() => {
-            navigation.navigate("Register"); //poner el nombre del archivo
-          }}
-        />
-      </View>
+          <ButtonPrimary
+            title="login"
+            text="Iniciar sesión"
+            onPress={handleSubmit(onSubmit)}
+          />
+          <ButtonPrimary
+            title="register"
+            text="Registrarse"
+            onPress={() => {
+              navigation.navigate("Register");
+            }}
+          />
+        </View>
       </TouchableWithoutFeedback>
     </SimpleLayout>
   );
